9e631cb398c5f3d3aac580415bde1cfaa15e1b86
Alisa <chenalisa2001@gmail.com>
2022-11-07 22:21:33 +0300

    clean

--- app/src/main/java/com/example/homework1/MainActivity.kt
+++ app/src/main/java/com/example/homework1/MainActivity.kt
@@ -4,32 +4,28 @@ package com.example.homework
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.os.Parcel
-import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProviders
 import com.example.homework1.R
-import kotlinx.android.synthetic.main.fragment.view.*
 
 
 
-class MyFragment(index: Int) : Fragment() {
-    private val index: Any = index
-
+class MyFragment(i: Int) : Fragment() {
+    private val index: Any = i
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view: View = inflater.inflate(R.layout.fragment, container, false)
-        view.textView.text = index.toString()
+        val text: TextView = view.findViewById(R.id.textView)
+        text.setText(index.toString())
         return view
     }
 }
