3187d03bef620f6e0ce4e2a627fabcfa16269685
Alisa <chenalisa2001@gmail.com>
2022-11-11 04:31:11 +0300

    checj

--- app/src/main/java/com/example/homework1/MainActivity.kt
+++ app/src/main/java/com/example/homework1/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.homework
 //noinspection SuspiciousImport
 
 import android.annotation.SuppressLint
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,11 +13,11 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProviders
 import com.example.homework1.MyViewModel
 import com.example.homework1.R
-
 
 
 class MyFragment(i: Int) : Fragment() {
@@ -29,26 +31,42 @@ class MyFragment(i: Int) : Fragment() {
         text.setText(index.toString())
         return view
     }
-}
 
+}
+object FragmentData {
+    var frList : ArrayList<MyFragment> = arrayListOf()
+    public fun getList(): ArrayList<MyFragment> {
+        return frList
+    }
+    fun addFragment(m: MyFragment) {
+        frList.add(m)
+    }
+}
 
 class MainActivity : AppCompatActivity() {
     private val fragmentManager = supportFragmentManager
+    val modelFr: MyViewModel = ViewModelProviders.of(this).get(MyViewModel::class.java)
+
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val model: ViewModel = ViewModelProviders.of(this).get(MyViewModel::class.java)
 
         setContentView(R.layout.activity_main)
+
         val button: Button = findViewById(R.id.button)
         var i = 0
         button.setOnClickListener {
             var fragmentTransaction = fragmentManager.beginTransaction()
             fragmentTransaction.add(R.id.fragment_container_view1, MyFragment(i))
             fragmentTransaction.commit()
+            FragmentData.addFragment(MyFragment(i))
             i++
         }
+    }
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        modelFr.updateModel()
     }
 }
 
